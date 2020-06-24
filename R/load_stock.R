4d5796c481d9531e56289c393f587a71a0cd4c01
shill1729 <52792611+shill1729@users.noreply.github.com>
2020-06-24 17:35:40 -0400

    add arithmetic returns function

--- R/load_stock.R
+++ R/load_stock.R
@@ -9,3 +9,30 @@ load_stock <- function(ticker)
   s <- quantmod::getSymbols(Symbols = ticker,src = "av", auto.assign = FALSE, output.size = "full",  api.key = Sys.getenv("api.key"))
   return(s)
 }
+
+#' Load multiple stocks
+#'
+#' @param tickers the stock tickers to load
+#' @param call_rate how long to wait between groups of API calls to Alpha Vantage
+#'
+#' @return see quantmod \code{getSymbols}
+#' @export load_stocks
+load_stocks <- function(tickers, call_rate = 60)
+{
+  # Function
+  stock_list <- list()
+  N <- length(tickers)
+  for(i in 1:N)
+  {
+    if(i%%5 == 0)
+    {
+      print("Free AV calls are limited, waiting...")
+      Sys.sleep(call_rate)
+    }
+    print(tickers[i])
+    stock_list[[i]] <- trader::load_stock(ticker = tickers[i])
+  }
+  stocks <- do.call(cbind, args = lapply(stock_list, quantmod::Cl))
+  stocks <- stocks[stats::complete.cases(stocks),]
+  return(stocks)
+}
---
+++ R/returns.R
@@ -0,0 +1,12 @@
+#' One-period arithmetic returns of a given time-series
+#'
+#' @param s the time-series of prices
+#'
+#' @description {The daily arithmetic returns between two periods is the difference of the prices over the
+#' two periods divided by the first period's price.}
+#' @return vector
+#' @export arithmetic_return
+arithmetic_return <- function(s)
+{
+  diff(s)/(s[1:(length(s)-1)])
+}
