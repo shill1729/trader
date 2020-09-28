f0d747afabad5885cdb75fdb211c6ee6f883a3ff
shill1729 <52792611+shill1729@users.noreply.github.com>
2020-09-28 12:13:11 -0400

    add dtfm

--- R/dtfm.R
+++ R/dtfm.R
@@ -4,8 +4,8 @@
 #' @param distr distribution name: "norm" or "unif"
 #'
 #' @return vector
-#' @export fitDTFM
-fitDTFM <- function(x, distr = "norm")
+#' @export fit_dtfm
+fit_dtfm <- function(x, distr = "norm")
 {
   if(distr == "unif")
   {
@@ -23,10 +23,33 @@ fitDTFM <- function(x, distr = "norm")
 #' @param param parameters of distribution as a vector
 #'
 #' @return numeric
-#' @export dDTFM
-dDTFM <- function(r, distr = "norm", param)
+#' @export ddtfm
+ddtfm <- function(r, distr = "norm", param)
 {
   ddistr <- paste("d", distr, sep = "")
   ddistr <- get(ddistr)
   return(do.call(ddistr, args = c(list(r), as.list(param))))
+}
+
+#' Fit a discrete time daily returns distribution and get log-optimal allocation
+#'
+#' @param symbol stock ticker to look up
+#' @param distr distribution to fit: "unif", "norm", more to come...
+#' @param rate risk-neutral rate
+#'
+#' @description {Looks up a stock's history on AV, estimates a return distribution
+#' for daily-arithmetic returns, and then computes the optimal log utility criterion.
+#' }
+#' @return list of fitted parameters, optimal allocation, and optimal growth (annualized)
+#' @export dtfm
+dtfm <- function(symbol, distr = "unif", rate = 0)
+{
+  rrate <- (1+rate)^(1/252)-1
+  s <- time_series(symbol, "daily")
+  x <- daily_returns(s$adj_close)$arithmetic
+  param <- fit_dtfm(x, distr)
+  kelly <- KellyCriterion::kelly_dtfm(distr, param, rrate)
+  entropy <- KellyCriterion::entropy_dtfm(distr, param, rrate)*252
+  # print(KellyCriterion::sim_dtfm(n, as.numeric(s$adj_close[1]), 1800, distr, param, rrate))
+  return(list(param = param, kelly = kelly, entropy = entropy))
 }
