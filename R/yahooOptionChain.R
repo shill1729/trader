80c5ee75cc23fc7cdf6b9e047aaebb70b39b199c
shill1729 <52792611+shill1729@users.noreply.github.com>
2021-04-28 14:41:13 -0400

    add getYahooOptionChain

--- R/yahooOptionChain.R
+++ R/yahooOptionChain.R
@@ -1,26 +1,39 @@
-# library(trader)
-# symbol <- "SPY"
-# option_type <- "puts"
-# #=========================================================
-# spot <- trader::getQuoteAV(symbol)$price
-# optionChain <- quantmod::getOptionChain(symbol, Exp = NULL)
-# #=========================================================
-# # Error check, sometimes Yahoo has no data intermittenly
-# if(is.null(names(optionChain)))
-# {
-#   stop(paste("No expiries returned for", symbol))
-# }
-# # Convert date-format from Month.dd.yyyy to yyyy-mm-dd
-# expiries <- gsub("\\.", "-", names(optionChain))
-# expiries <- as.Date(expiries, format = "%b-%d-%Y")
-# maturity <- trader::date_yte(expiries) # convert to YTE
-# # Loop through all expiries and merge by strike, for given payoff-type
-# optionSurface <- lapply(optionChain, function(x) data.frame(Strike = x[[option_type]]$Strike, Last = x$puts$Last))
-# # Suppress duplicate column warning
-# suppressWarnings(optionSurface <- Reduce(function(...) merge(..., all = TRUE, by="Strike"), optionSurface))
-# optionSurface <- optionSurface[complete.cases(optionSurface), ]
-# colnames(optionSurface) <- c("Strike", as.character(expiries))
-# # Check output
-# print(optionSurface)
-# plot3D::persp3D(z = as.matrix(optionSurface)[,-1], phi = 10, theta = 60)
-
+#' Download option-chain from Yahoo-Finance
+#'
+#' @param symbol stock-ticker symbol to look up
+#' @param option_type "puts" or "calls"
+#' @param plotSurface boolean for plotting the 3D raw price-surface
+#'
+#' @description {Download option chains for all available expiries of a given stock ticker,
+#' then merge them by common strike prices into one matrix/data.frame. This is essentially
+#' a wrapper to \code{quantmod::getOptionChain} with some processing/formatting
+#' added afterwards.}
+#' @return data.frame
+#' @export getYahooOptionChain
+getYahooOptionChain <- function(symbol, option_type = "calls", plotSurface = FALSE)
+{
+  #=========================================================
+  spot <- trader::getQuoteAV(symbol)$price
+  z <- quantmod::getOptionChain(symbol, Exp = NULL)
+  #=========================================================
+  # Error check, sometimes Yahoo has no data intermittently
+  if(is.null(names(z)))
+  {
+    stop(paste("No expiries returned for", symbol))
+  }
+  # Convert date-format from Month.dd.yyyy to yyyy-mm-dd
+  expiries <- gsub("\\.", "-", names(z))
+  expiries <- as.Date(expiries, format = "%b-%d-%Y")
+  maturity <- trader::date_yte(expiries) # convert to YTE
+  # Loop through all expiries and merge by strike, for given payoff-type
+  marketData <- lapply(z, function(x) data.frame(Strike = x[[option_type]]$Strike, Last = x[[option_type]]$Last))
+  # Suppres duplicate column warning
+  suppressWarnings(marketData <- Reduce(function(...) merge(..., all = TRUE, by="Strike"), marketData))
+  marketData <- marketData[complete.cases(marketData), ]
+  colnames(marketData) <- c("Strike", as.character(expiries))
+  if(plotSurface)
+  {
+    plot3D::persp3D(z = as.matrix(marketData)[,-1], phi = 10, theta = 60)
+  }
+  return(marketData)
+}
